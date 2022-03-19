9360a46bb9b55463ca362a2fb71f0a16b0355200
MohamedHeshamAli <aliheshamaliali2@gmail.com>
2022-03-19 21:41:13 +0200

    project initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:my_store/pages/login/login_screen.dart';
-
+//
 
 void main() {
   runApp(const MyApp());
